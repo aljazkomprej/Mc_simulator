42a3c0d27e45de8b5db5edc5a1edf0bc1e98a432
David Stojan <david.stojan@si.mahle.com>
2018-01-24 09:33:55 +0100

    Fix path of kst plotting batch file
    In batch file kst program PATH must be updated.

--- run_kst_plot.bat
+++ run_kst_plot.bat
@@ -4,6 +4,6 @@
 :: Define path of your KST \bin folder
 set PATH=%PATH%;D:\Programs\Kst-2.0.x-2017.08.19-00.34-win32\bin
 
-pushd D:\stojand\Projekti\WaPu_FOC\c programming\mcsim20171003_jc_ds\mcsim20171003_jc
+pushd %~dp0
 
 start kst2 kst_plot_inverter_slow.kst
